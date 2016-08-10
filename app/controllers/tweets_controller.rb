14080f8dc10a3ee1e2d1db76b48f7827d56dc747
miyamomo <miyamomo@gmail.com>
2016-08-10 17:38:27 +0900

    L5 deviseインストールと設定まで

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
     # @tweet = Tweet.find(1)
     # @tweets = Tweet.all.order("created_at DESC")
@@ -19,6 +21,10 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
